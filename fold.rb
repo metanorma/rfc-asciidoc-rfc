f4faba8eb55af1743a7a60ba5854a7cf35404d24
Nick Nicholas <opoudjis@optusnet.com.au>
2018-03-23 22:14:59 +1100

    fold

--- fold.rb
+++ fold.rb
@@ -1,11 +1,11 @@
 def wrap(s, width=78)
-  s.gsub(/(.{1,#{width}})(\s+|\Z)/, "\\1\n")
+  s.gsub(/(.{10,#{width}})(\s+|\Z)/, "\\1\n").sub(/^\s+/, "")
 end
 
-embed = false
+embed = 0
 $stdin.each_line do |line|
-  embed = true if /CODE BEGINS/.match? line
-  embed = false if /CODE ENDS/.match? line
-  line = wrap(line, 72) if embed
+  embed = embed+1 if /CODE BEGINS/.match? line
+  embed = embed-1 if /CODE ENDS/.match? line
+  line = wrap(line, 72) if embed>0 # && line.length > 72
   puts line
 end
