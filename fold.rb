596e49ce119a8573593f6496fbbd62c6872e3b52
Nick Nicholas <opoudjis@optusnet.com.au>
2018-04-09 21:31:05 +1000

    improved fold script

--- fold.rb
+++ fold.rb
@@ -3,9 +3,12 @@ def wrap(s, width=78)
 end
 
 embed = 0
+cdata = false
 $stdin.each_line do |line|
   embed = embed+1 if /CODE BEGINS/.match? line
+  cdata = true if line.include? "<![CDATA["
+  cdata = false if cdata && line.include?(']]>')
   embed = embed-1 if /CODE ENDS/.match? line
-  line = wrap(line, 72) if embed>0 # && line.length > 72
+  line = wrap(line, 72) if embed>0 && !cdata # && line.length > 72
   puts line
 end
